ed0f018c85811328c44a8e40b038168293b1aa46
Altair-GM <ghannou.0702@gmail.com>
2022-01-21 16:20:36 +0100

    integrtion template

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+    
+        <title>Elegance - Creative HTML5 Template</title>
+    <!--
+    Elegance Template
+    https://templatemo.com/tm-528-elegance
+    -->
+        <!-- Additional CSS Files -->
+        <link rel="stylesheet" type="text/css" href="{{ asset ("css/bootstrap.min.css") }}">
+    
+        <link rel="stylesheet" type="text/css" href="{{ asset ("css/font-awesome.css") }}">
+    
+        <link rel="stylesheet" type="text/css" href="{{ asset ("css/fullpage.min.css") }}">
+    
+        <link rel="stylesheet" type="text/css" href="{{asset ("css/owl.carousel.css") }}">
+    
+        <link rel="stylesheet" href="{{ asset ("css/animate.css") }}">
+    
+        <link rel="stylesheet" href="{{ asset ("css/templatemo-style.css") }}">
+    
+        <link rel="stylesheet" href="{{ asset ("css/responsive.css") }}">
+    
+        </head>
+<body>
+    @yield('content')
+    <script src="{{ asset ("js/jquery.js") }}"></script>
+
+    <script src="{{ asset ("js/bootstrap.min.js") }}"></script>
+
+    <script src="{{ asset ("js/fullpage.min.js") }}"></script>
+
+    <script src="{{ asset ("js/scrolloverflow.js") }}"></script>
+
+    <script src="{{ asset ("js/owl.carousel.min.js") }}"></script>
+
+    <script src="{{ asset ("js/jquery.inview.min.js") }}"></script>
+
+    <script src="{{ asset ("js/form.js") }}"></script>
+
+    <script src="{{ asset ("js/custom.js") }}"></script>
+</body>
+</html>
